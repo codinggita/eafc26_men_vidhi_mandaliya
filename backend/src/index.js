a6d7af3384b74f3a5b24e18e5e0dab20c2539d75
Vidhi2807 <vidhimandaliya81@gmail.com>
2026-06-02 17:33:03 +0530

    add mongodb connection configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -21,5 +23,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
